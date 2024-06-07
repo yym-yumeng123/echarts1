92debb7337e79a54d1aac72181e9affea8ebf97b
yym-yumeng123 <18026493873@163.com>
2024-06-07 15:18:19 +0800

    feat: k线图优化

--- src/components/candlestick/Candlestick.vue
+++ src/components/candlestick/Candlestick.vue
@@ -20,6 +20,13 @@ onMounted(() => {
     yAxis: {
       type: "value",
     },
+    tooltip: {
+      show: true,
+      trigger: "axis",
+      axisPointer: {
+        type: "cross",
+      },
+    },
     series: [
       {
         type: "candlestick", // K 线图
@@ -27,11 +34,34 @@ onMounted(() => {
           [100, 200, 80, 150], // 开盘, 收盘, 最低, 最高
           [150, 220, 70, 200],
           [200, 250, 150, 220],
-          [180, 220, 150, 200],
+          [180, 120, 150, 200],
           [200, 300, 180, 250],
           [250, 280, 200, 250],
-          [300, 320, 250, 300],
+          [300, 220, 250, 300],
         ],
+        itemStyle: {
+          color: "#ec0000", // 阳线颜色
+          color0: "#00da3c", // 阴线颜色
+          borderColor: "#8A0000", // 阳线边框颜色
+          borderColor0: "#008F28", // 阴线边框颜色
+        },
+        markPoint: {
+          data: [
+            { type: "max", name: "最大值", valueDim: "highest" }, // 最高值的维度, 默认为最后一个维度
+            { type: "min", name: "最小值", valueDim: "lowest" }, // 最低值的维度, 默认为最后一个维度
+            { type: "average", name: "平均值" },
+          ],
+        },
+      },
+      {
+        type: "line",
+        data: [150, 220, 250, 120, 300, 280, 220], // 收盘价
+        smooth: true, // 平滑曲线
+        markPoint: {
+          data: [
+            { name: "周最低", value: -2, xAxis: 1, yAxis: 260 },
+          ],
+        },
       },
     ],
   });
