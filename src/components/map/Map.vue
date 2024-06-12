29f7a252450284549bac9e25663613eade2f2c12
yym-yumeng123 <18026493873@163.com>
2024-06-12 14:57:34 +0800

    fix: 安徽地图 json

--- src/components/map/Map.vue
+++ src/components/map/Map.vue
@@ -1,6 +1,9 @@
 <!-- 中国地图 -->
 <script setup>
+// 中国地图json 数据
 import mapJson from '../../assets/map.json'
+// 安徽地图json 数据
+import anhuiJson from '../../assets/anhui.json'
 import * as echarts from "echarts";
 import { onMounted, ref } from "vue";
 const myChart = ref(null);
@@ -8,7 +11,7 @@ const myChart = ref(null);
 onMounted(() => {
   const myEcharts = echarts.init(myChart.value);
   // 中国地图 注册地图 json
-  echarts.registerMap("china", mapJson);
+  echarts.registerMap("china", anhuiJson);
   myEcharts.setOption({
     title: {
       text: "ECharts 入门示例",
@@ -26,7 +29,7 @@ onMounted(() => {
       map: "china",
       roam: true, // 开启缩放和平移
       zoom: 1.2, // 缩放比例
-      center: [104.114129, 37.550339], // 中心点
+      // center: [104.114129, 37.550339], // 中心点
       label: {
         normal: {
           show: true,
